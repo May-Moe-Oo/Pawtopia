762b27e0cbf403a6f72c9e64701f181daa3c1843
MayMoeOo95 <122252464+MayMoeOo95@users.noreply.github.com>
2023-04-16 17:57:44 +0800

    Project Setup
    npm create vite@latest
    npm run build
    npm install express
    npm install serve-favicon
    npm i dotenv
    npm install morgan
    npm i mongoose
    npm install bcrypt
    npm install jsonwebtoken
    added gitignore, .env, database, and others files and folders

---
+++ models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema;
+const SALT_ROUNDS = 6;
+
+const usersSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      unique: true,
+      trim: true,
+      minLength: 5,
+      required: true,
+    },
+    userRole: {
+      type: String,
+      required: true,
+      enum: ["customer", "admin"],
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+usersSchema.pre("save", async function (next) {
+  // 'this' is the user doc
+  if (!this.isModified("password")) return next();
+  // update the password with the computed hash pw
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  return next();
+});
+
+usersSchema.set("toJSON", {
+  transform: function (doc, ret) {
+    delete ret["password"];
+    return ret;
+  },
+});
+
+module.exports = mongoose.model("User", usersSchema);
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+
+require("dotenv").config();
+const database = require("./config/database");
+database.connect();
+
+const userRouter = require("./routes/usersRouter");
+//! add other routers here
+
+const jwt = require("jsonwebtoken");
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
+
+// app.get("/api", (req, res) => {
+//   res.json({ msg: "Hi" });
+// });
+
+const isLoggedIn = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    const token = authorization.split(" ")[1];
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    if (decode) {
+      res.locals.user = decode.user;
+      next();
+    } else {
+      res.status(403).json({ message: "Sorry" });
+    }
+  } catch (error) {
+    res.status(403).json({ error });
+  }
+};
+
+app.use("/api/users", userRouter);
+//! add other routes here
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+setTimeout(() => {
+  app.listen(port, () => {
+    console.log(`Express listening on port ${port}`);
+  });
+}, 1000); // add a delay of 3 seconds before starting the server
+
+module.exports = app;
