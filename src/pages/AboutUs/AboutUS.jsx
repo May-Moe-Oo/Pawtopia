cc3a47e77ffbd4a355b3f5cb9d3f41cbf1af9686
MayMoeOo95 <122252464+May-Moe-Oo@users.noreply.github.com>
2023-04-18 17:53:08 +0800

    remove data-theme="cupcake" at AboutUs.jsx

--- src/pages/AboutUs/AboutUS.jsx
+++ src/pages/AboutUs/AboutUS.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function AboutUs() {
     return (
         <div>
-            <h1 data-theme="cupcake">A Perfect Pawcation for our Paw Buddies </h1>
+            <h1>A Perfect Pawcation for our Paw Buddies </h1>
             <h2>Find your pawfect room</h2> {/* room should be link to room page */}
             <h2>Get in Touch with Us! </h2> {/* Us should be link to contact us page */}
             <h2>If you have any inquiry, feel free to send us your message.</h2> 
